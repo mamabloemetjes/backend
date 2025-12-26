6df74061a3ab24793f020f7c319184567c006194
monky <levinoppers@proton.me>
2025-12-26 23:46:52 +0100

    add memory to health stats

--- services/health_service.go
+++ services/health_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"mamabloemetjes_server/database"
+	"runtime"
 	"time"
 
 	"github.com/MonkyMars/gecho"
@@ -18,6 +19,14 @@ type serverHealthStatus struct {
 	Uptime       float64   `json:"uptime"`        // in seconds
 	CurrentTime  time.Time `json:"current_time"`  // server current time
 	ServiceAlive bool      `json:"service_alive"` // always true if service is running
+	RamStats     *RamStats `json:"ram_stats"`
+}
+
+type RamStats struct {
+	TotalMB     uint64 `json:"total_mb"`
+	UsedMB      uint64 `json:"used_mb"`
+	FreeMB      uint64 `json:"free_mb"`
+	UsedPercent uint64 `json:"used_percent"`
 }
 
 type databaseHealthStatus struct {
@@ -40,13 +49,35 @@ func NewHealthService(logger *gecho.Logger, db *database.DB) *HealthService {
 			Uptime:       0,
 			CurrentTime:  time.Now(),
 			ServiceAlive: true,
+			RamStats:     getRamStats(),
 		},
+	}
+}
+
+func getRamStats() *RamStats {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	totalMB := m.Sys / 1024 / 1024
+	usedMB := m.Alloc / 1024 / 1024
+	freeMB := totalMB - usedMB
+	usedPercent := uint64(0)
+	if totalMB > 0 {
+		usedPercent = (usedMB * 100) / totalMB
+	}
+
+	return &RamStats{
+		TotalMB:     totalMB,
+		UsedMB:      usedMB,
+		FreeMB:      freeMB,
+		UsedPercent: usedPercent,
 	}
 }
 
 func (hs *HealthService) GetServerHealthStatus() serverHealthStatus {
 	hs.status.Uptime = time.Since(uptimeStart).Seconds()
 	hs.status.CurrentTime = time.Now()
+	hs.status.RamStats = getRamStats()
 	return hs.status
 }
 
